dcaed50c5c9e3925258f21998fc98dccdb502ecc
C₈H₁₀N₄O₂ <36227942+Fredrik1ST@users.noreply.github.com>
2020-02-24 16:43:32 +0100

    Bugfixes. Completed status retrieval, file selection, job selection.

--- OctoPrintCommunicator.py
+++ OctoPrintCommunicator.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import logging
 import json
 import requests
-import pandas as pd
 
 '''
 This class contains the necessary commands to extract information from Raspberry Pis running Octoprint,
@@ -13,59 +12,87 @@ JSON objects. Methods primarily return JSON-formatted strings.
 
 class OctoPrintClient:
 
-    def __init__(self, ipAddress, apiKey):
+    def __init__(self, ipAddress, apiKey, username, password):
         '''
         Initialize a "client". Each client handles one connection to one printer.
         A logger object is initialized to write error logs as well.
         '''
         self.ipAddress = ipAddress
         self.apiKey = apiKey
+        self.username = username
+        self.password = password
 
-        logging.basicConfig(filename='Log.txt', level=logging.DEBUG,
+        logging.basicConfig(filename='Log.txt', level=logging.ERROR,
                             format='%(asctime)s %(levelname)s %(name)s %(message)s')
         self.logger = logging.getLogger(__name__)
 
 
-    def login(self, username, password):
+    def printDebugInfo(self):
         '''
-        Log into octoprint on the specified IP address
+        Print relevant info about this object for debugging purposes
+        '''
+        print("IP: " + self.ipAddress + ", ")
+        print("API key: " + self.apiKey + ", ")
+        print("Username: " + self.username + ", ")
+        print("Password: " + self.password + ", ")
+
+    def login(self):
+        '''
+        Log into Octoprint on the specified IP address
+        Returns response as string.
         '''
         url = "http://" + self.ipAddress + "/api/login"
-        payload = {"user": username, "pass": password}
-        r = requests.post(url, data=payload)
+        payload = {"user": self.username, "pass": self.password}
+        r = requests.post(url, json=payload)
         return r.text
 
+    def logout(self):
+        '''
+        Log out from Octoprint on the specified IP address.
+        You probably do not need to use this for this program.
+        Returns response as string.
+        '''
+        url = "http://" + self.ipAddress + "/api/logout"
+        r = requests.post(url)
+        return r.text
 
     def connectToPrinter(self):
         '''
         Connect to the 3D printer over USB. Default values are used.
-        Arguments: Octopi IP address, Octopi API key
+        Returns response as string.
         '''
         url = "http://" + self.ipAddress + "/api/connection"
         headers = {"Content-Type": "application/json", "X-Api-Key": self.apiKey}
         payload = {"command": "connect"}
-        r = requests.post(url, headers=headers, data=payload)
-        print(r)
+        r = requests.post(url, headers=headers, json=payload)
+        return r.text
 
+    def disconnectFromPrinter(self):
+        '''
+        Disconnect from the 3D printer if connected.
+        Returns response as string.
+        '''
+        url = "http://" + self.ipAddress + "/api/connection"
+        headers = {"Content-Type": "application/json", "X-Api-Key": self.apiKey}
+        payload = {"command": "connect"}
+        r = requests.post(url, headers=headers, json=payload)
+        return r.text
 
     def isPrinterConnected(self):
         '''
         Check if the specified Pi is connected to the Printer.
-        Arguments: Octopi IP address, Octopi API Key
         Returns: True if connected, False if not
         '''
         url = "http://" + self.ipAddress + "/api/connection"
         headers = {"X-Api-Key": self.apiKey}
-        try:
-            r = requests.get(url, headers=headers)
-            rJson = json.loads(r.text)
 
-            if (rJson["current"]["state"]) == "Operational": # Can be Closed or Operational
-                return True
-            else:
-                return False
-        except Exception as e:
-            self.logger.error(e)
+        r = requests.get(url, headers=headers)
+        rJson = json.loads(r.text)
+
+        if (rJson["current"]["state"]) == "Operational": # Can be Closed or Operational
+            return True
+        else:
+            return False
 
 
     def getPrinterStatus(self):
@@ -83,15 +110,37 @@ class OctoPrintClient:
         else:
             return r.text
 
+    def getCurrentPrintJob(self):
+        '''
+        Check to see what is currently printing, if anything at all.
+        Return JSON string containing job info.
+        '''
+        url = "http://" + self.ipAddress + "/api/job"
+        headers = {"X-Api-Key": self.apiKey}
+        r = requests.get(url, headers=headers)
+        return r.text
+
+    def selectPrintJob(self, gcodePath):
+        '''
+        Select gcode stored on the Pi.
+        Argument: path to the gcode
+        Returns the response as JSON string
+        '''
+        url = "http://" + self.ipAddress + gcodePath
+        headers = {"Content-Type": "application/json", "X-Api-Key": self.apiKey}
+        payload = {"command": "select"}
+        r = requests.post(url, headers=headers, json=payload)
+        return r.text
 
     def startPrintJob(self):
         '''
         Tell the specified printer to start printing the selected G-code
         G-code should be uploaded to the Manulab-folder in octoprint in advance.
         Arguments: Octopi IP address, Octopi API key, path to g-code file on the pi
+        Returns: response as JSON string
         '''
         url = "http://" + self.ipAddress + "/api/job"
         headers = {"Content-Type": "application/json", "X-Api-Key": self.apiKey}
         payload = {"command": "start"}
-        r = requests.post(url, headers=headers, data=payload)
-        print(r)
+        r = requests.post(url, headers=headers, json=payload)
+        return r.text
--- __main__.py
+++ __main__.py
@@ -6,28 +6,32 @@ import json
 import csv
 
 '''
-In this module, a list of IP addresses and API keys for Octoprint-connected 3D printers are read, then used for
-communication with other equipment. 
+In this module, a list of IP addresses and API keys for Octoprint-connected 3D printers are read, then used to create 
+instances of OctoPrintCommunicator clients. The OPCs handle communication between printers and different equipment.
 '''
 
 # Set up necessary variables
 opcs = list()                                       # For storing all initialized OctoPrintClient objects
 path_ListOfPrinters = Path("ListOfPrinters.csv")    # System-independent path
+printDebug = True;                                  # Toggle whether or not to print all responses to console
 
 # Set up logger
 logging.basicConfig(
-    filename='Log.txt', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    filename='Log.txt', level=logging.ERROR, format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
 
 
 def importPrinterList():
     '''
     Import Octopi / printer IP addresses and API keys from the local ListOfPrinters.csv
-    First row in the CSV must be "ipAddress,apiKey". The delimiter sign is automatically inferred.
+    Usernames and passwords for the Octoprint user on each Pi is included as well.
+    First row in the CSV must be "ipAddress,apiKey,username,password". The delimiter sign is automatically inferred.
     All valid rows are used to create OctoPrintClient objects, which are then stored in a list.
     '''
     global ipList
     global apiList
+    global usernameList
+    global passwordList
     try:
 
         # Infer CSV delimiter (comma or semicolon)
@@ -39,38 +43,58 @@ def importPrinterList():
         df = pd.read_csv(path_ListOfPrinters, sep=delimiter, header=0)
         ipList = list(df.ipAddress)
         apiList = list(df.apiKey)
+        usernameList = list(df.username)
+        passwordList = list(df.password)
 
         # Create an OPC instance for every element in the List Of Printers
         for i in range(len(ipList)):
-            opcs.append(opc.OctoPrintClient(ipList[i], apiList[i]))
+            opcs.append(opc.OctoPrintClient(ipList[i], apiList[i], usernameList[i], passwordList[i]))
 
     except Exception as e:
         logger.error(e)
         logger.error(print("ListOfPrinters.csv may be missing or of invalid format"))
 
-
-def getJsonNameValuePair(jsonObj, name, value=None):
+def updatePrinterStatus():
     '''
-    TODO: Test this when relevant OPC methods are done
-    What it says on the tin. Limited to simple structures with one name-value pair.
+    Read status from all available printers. Write desired data to one txt per printer to be parsed by PLC.
     '''
-    return (str(jsonObj[name]) + str(jsonObj[name][value]))
+    for i, opc in enumerate(opcs):
+        if opc.isPrinterConnected():
+            r = opc.login()
+        else:
+            r = opc.connectToPrinter()
+        opcStatus = opc.getPrinterStatus()
+        opcSJ = json.loads(opcStatus)
+        # Row structure: [printing] ; [ready] ; [operational] ; [pausing] ; [paused] ; [finishing] ; [nozzle temp] ; [bed temp]
+        opcStatusNames = ("Printing;Ready;Operational;Pausing;Paused;Finishing;NozzleTemp;BedTemp\n")
+        opcStatusString = (str(opcSJ['state']['flags']['printing']) + ';' +
+                           str(opcSJ['state']['flags']['ready'])    + ';' +
+                           str(opcSJ['state']['flags']['operational']) + ';' +
+                           str(opcSJ['state']['flags']['pausing'])  + ';' +
+                           str(opcSJ['state']['flags']['paused'])   + ';' +
+                           str(opcSJ['state']['flags']['finishing']) + ";" +
+                           str(opcSJ['temperature']['bed']['actual']) + ';' +
+                           str(opcSJ['temperature']['tool0']['actual']))
 
-# Test code:
-i = 0
+        # Write status to txt for each OPC instance
+        path_statusTxt = Path("PrinterStatus/" + "printer" + str(i) + ".txt")
+        with open(path_statusTxt, "w+") as statusTextFile:
+            statusTextFile.write(opcStatusNames + opcStatusString)
+
+        # Print responses if the printResponses debugging variable is set to true
+        if printDebug:
+            print(opc.ipAddress + " login / connection response:\n" + r)
+            print(opc.ipAddress + " printer status: " + opcStatus)
+
+
+# Test code
 
 importPrinterList()
-###print(opcs[i].login("USERNAME", "PASSWORD"))###
+updatePrinterStatus()
 
-opcStatus = opcs[i].getPrinterStatus()
-print(opcStatus)
-opcSJ = json.loads(opcStatus)
-# Row structure: [printing] ; [ready] ; [operational] ; [pausing] ; [paused] ; [finishing] ; [nozzle temp] ; [bed temp]
-opcStatusString = (str(opcSJ['state']['flags']['printing'])    + ';' + str(opcSJ['state']['flags']['ready'])      + ';' +
-                str(opcSJ['state']['flags']['operational']) + ';' + str(opcSJ['state']['flags']['pausing'])    + ';' +
-                str(opcSJ['state']['flags']['paused'])      + ';' + str(opcSJ['state']['flags']['finishing'])  + ";" +
-                str(opcSJ['state']['flags']['paused'])      + ';' + str(opcSJ['temperature']['bed']['actual']) + ';' +
-                str(opcSJ['temperature']['tool0']['actual']))
-
-print(opcStatusString)
-# opcs[i].startPrintJob() #TODO: find out why file and job posts receive HTTP response 400
+'''
+i = 0; # Test using first printer in OPC list
+print(opcs[i].selectPrintJob("/api/files/PATH TO GCODE IN OCTOPRINT"))
+#print(opcs[i].startPrintJob())
+#print(opcs[i].getCurrentPrintJob())
+'''
